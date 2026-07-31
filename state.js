2edc3344fd6d132786e057fd552cb03044864f25
izokizing <izokizing@gmail.com>
2026-07-31 14:48:30 +0300

    Update state.js

--- state.js
+++ state.js
@@ -3,3 +3,39 @@ const state = {
     filter: "all",
     theme: "light"
 };
+
+const createStore = (initialState) => {
+    let state = initialState;
+    const listeners = [];
+
+    return {
+        getState: () => state,
+
+        setState: (updates) => {
+            state = { ...state, ...updates };
+            listerners.forEach(listener => listener(state));
+
+        },
+
+        subscribe: (listener) => {
+            listeners.push(listener);
+
+            return () => {
+                const index = listeners.indexOf(listener);
+                listener.splice(index, 1);
+            };
+        }
+    };
+};
+
+const store = createStore({ count: 0 });
+
+const unsubscribe = store.subscribe(state => {
+    console.log("State changed:", state);
+    renderUI(state);
+});
+
+store.setState({ count: 1 });
+store.setState({ count: 2 });
+
+unsubscribe();
