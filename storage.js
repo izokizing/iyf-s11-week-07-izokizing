79f368ea275c9acd449394b46d2c7362915454ca
izokizing <izokizing@gmail.com>
2026-07-31 14:42:16 +0300

    Update storage.js

--- storage.js
+++ storage.js
@@ -73,3 +73,49 @@ function removeFromStorage(key) {
 }
 
 let notes = getFromStorage("notes");
+
+
+const STORAGE_KEY = "todos";
+
+function loadTodos() {
+    return getFromStorage(STORAGE_KEY, []);
+
+}
+
+function saveTodos(todos) {
+    seveToStorage(STORAGE_KEY, todos);
+}
+
+function addTodo(text) {
+    const newTodo = {
+        id: Date.now(),
+        text: text,
+        completed: false,
+        createdAt: new Date().toISOString()
+    };
+
+    const todos = loadTodos();
+    todos.push(newTodo);
+    saveTodos(todos);
+
+    renderTodos();
+}
+
+function toggleTodo(id) {
+    const todos = loadTodos();
+    const todo = todos.find(t => t.id === id);
+
+    if (todo) {
+        todo.completed = !todo.completed;
+        saveTodos(todos);
+        renderTodos();
+    }
+}
+
+function deleteTodo(id) {
+    let todos = loadTodos();
+    todos = todos.filter(t => t.id !== id);
+    saveTodos(todos);
+    renderTodos();
+}
+
