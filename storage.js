f3583a4b5a8e03db99ae1dd5a2013cb7f0171f95
izokizing <izokizing@gmail.com>
2026-07-31 14:47:22 +0300

    Update storage.js

--- storage.js
+++ storage.js
@@ -119,3 +119,10 @@ function deleteTodo(id) {
     renderTodos();
 }
 
+function loadState() {
+    const saved = localStorage.getItem("appState");
+    if (saved) {
+        Object.assign(state, JSON.parse(saved));
+    }
+}
+
