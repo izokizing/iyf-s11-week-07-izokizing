1fc61a4524200e9221dfd4a40a9de26b0d7df042
izokizing <izokizing@gmail.com>
2026-08-02 15:40:31 +0300

    Remove renderTodos function and filtering logic
    Removed the renderTodos function and its related logic.

--- app.js
+++ app.js
@@ -79,24 +79,7 @@ function createTodoElement(todo) {
    return li;
 }
 
-function renderTodos() {
-    todoList.innerHTML ="";
 
-    let filteredTodos = todos;
-
-    if (currentFilter === "active") {
-        filtererdTodos = todos.filter(todo => !todo.completed);
-    }
-    else if (currentFilter === "completed") {
-        filteredTodos = todos.filter(todo => todo.completed);
-    }
-
-    filteredTodos.forEach(function(todo) {
-        const li = createTodoElement(todo);
-        todoList.appendChild(li);
-    });
-    updateStats();
-}
 
 filters.forEach(function (button) {
     button.addEventListener("click", function() {
